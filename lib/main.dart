525d679887c9d5debd9f97613fa6c956173025b2
DevelopedByDev <dlalwan1@jhu.edu>
2025-04-30 14:45:36 -0400

    added record_hotkey_dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:autoquill_ai/features/transcription/presentation/pages/transcription_page.dart';
 import 'package:autoquill_ai/features/transcription/presentation/bloc/transcription_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bot_toast/bot_toast.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hotkey_manager/hotkey_manager.dart';
 import 'package:path_provider/path_provider.dart';
 import 'core/di/injection_container.dart' as di;
 import 'core/storage/app_storage.dart';
@@ -11,16 +14,25 @@ import 'features/transcription/domain/repositories/transcription_repository.dart
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   // Initialize Hive
   final appDir = await getApplicationDocumentsDirectory();
   await Hive.initFlutter(appDir.path);
   await AppStorage.init();
-  
+
   // Initialize dependency injection
   await di.init();
-  
+
   runApp(const MainApp());
+}
+
+class ExampleIntent extends Intent {}
+
+class ExampleAction extends Action<ExampleIntent> {
+  @override
+  void invoke(covariant ExampleIntent intent) {
+    print('ExampleAction invoked');
+  }
 }
 
 class MainApp extends StatelessWidget {
@@ -28,33 +40,46 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'AutoQuill AI',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
-      ),
-      home: MultiRepositoryProvider(
-        providers: [
-          RepositoryProvider<TranscriptionRepository>(
-            create: (_) => di.sl<TranscriptionRepository>(),
+    return Actions(
+      actions: <Type, Action<Intent>>{
+        ExampleIntent: ExampleAction(),
+      },
+      child: GlobalShortcuts(
+        shortcuts: {
+          const SingleActivator(LogicalKeyboardKey.keyA, alt: true):
+              ExampleIntent(),
+        },
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'AutoQuill AI',
+          builder: BotToastInit(),
+          navigatorObservers: [BotToastNavigatorObserver()],
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            useMaterial3: true,
           ),
-        ],
-        child: MultiBlocProvider(
-          providers: [
-            BlocProvider(
-              create: (_) => RecordingBloc(
-                repository: di.sl(),
+          home: MultiRepositoryProvider(
+            providers: [
+              RepositoryProvider<TranscriptionRepository>(
+                create: (_) => di.sl<TranscriptionRepository>(),
               ),
+            ],
+            child: MultiBlocProvider(
+              providers: [
+                BlocProvider(
+                  create: (_) => RecordingBloc(
+                    repository: di.sl(),
+                  ),
+                ),
+                BlocProvider(
+                  create: (context) => TranscriptionBloc(
+                    repository: context.read(),
+                  )..add(InitializeTranscription()),
+                ),
+              ],
+              child: const TranscriptionPage(),
             ),
-            BlocProvider(
-              create: (context) => TranscriptionBloc(
-                repository: context.read(),
-              )..add(InitializeTranscription()),
-            ),
-          ],
-          child: const TranscriptionPage(),
+          ),
         ),
       ),
     );
